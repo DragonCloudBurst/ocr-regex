e9996cbf590eb7784a258716664e4982c1e10cba
SPIVEYA23 <spiveya23@gmail.com>
2025-11-18 18:01:08 -0500

    search for occurences of a space then punctuation, and remove them

--- ocrRegex.py
+++ ocrRegex.py
@@ -5,19 +5,21 @@ sample2 = "student turns 16,&quot; Dr. Delma Blinson, &nbsp;superintendent of th
 output = sample
 output2 = sample2
 
-input_text = open("files/example2.txt", "r")
+input_text = open("files/example3.txt", "r")
 output_text = input_text.read()
 output_file = open("files/output.txt", "w")
 
-patterns = ["&quot;", "&nbsp;", "&gt;", "&lt;", "&amp;"]
-replaces = ['"', " ", ">", "<", "&"]
+
+# trying to figure out how to remove a space then punctuation. for example if its
+# hi .i am
+# then it should remove " ." 
+patterns = ["[ \s][:\|.?]", "&quot;", "&nbsp;", "&gt;", "&lt;", "&amp;", "~", "�?", "�", "\|", "[ \s][?]"]
+replaces = [" ", '"', " ", "", "", "&", " ", "", "", " ", " "]
 replaces_counter = 0
 
-for pat in patterns:    
-    print(pat)
-    print(replaces_counter)
-    if pat in output_text:
-        output_text = re.sub(pat, replaces[replaces_counter], output_text)
+for pat in patterns:
+    #if pat in output_text:
+    output_text = re.sub(pat, replaces[replaces_counter], output_text)
     replaces_counter += 1
     
 # having trouble with removing carets via regex despite using escape chars. workaround.
