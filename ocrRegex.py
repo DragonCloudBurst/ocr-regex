1f86ee7c8fbea03ad740290cebdbc0a317994766
SPIVEYA23 <spiveya23@gmail.com>
2025-11-20 12:26:45 -0500

    implement typer for command-line argument passing + change output filenames

--- ocrRegex.py
+++ ocrRegex.py
@@ -1,33 +1,35 @@
 import re
+import typer
 
-sample = "student turns 16,&quot; Dr. Delma Blinson, superintendent of the city schools remarked. &quot;But the problem basically goes back"
-sample2 = "student turns 16,&quot; Dr. Delma Blinson, &nbsp;superintendent of the city schools remarked. ^^ &nbsp;But the problem basically goes back"
-output = sample
-output2 = sample2
+app = typer.Typer()
 
-input_text = open("files/example3.txt", "r")
-output_text = input_text.read()
-output_file = open("files/output.txt", "w")
+@app.command()
+def regExRun(filename: str):
+    input_text = open(f"files/{filename}", "r")
+    output_text = input_text.read()
+    output_file = open(f"files/output_{filename[:-4]}.txt", "w")
 
+    # ask joe: should a backslash ever be in text?
 
-# trying to figure out how to remove a space then punctuation. for example if its
-# hi .i am
-# then it should remove " ." 
-patterns = ["[ \s][:\|.?]", "&quot;", "&nbsp;", "&gt;", "&lt;", "&amp;", "~", "�?", "�", "\|", "[ \s][?]"]
-replaces = [" ", '"', " ", "", "", "&", " ", "", "", " ", " "]
-replaces_counter = 0
+    # trying to figure out how to remove a space then punctuation. for example if its
+    # hi .i am
+    # then it should remove " ." 
+    patterns = ["[ \s][:\|.?/]", "&quot;", "&nbsp;", "&gt;", "&lt;", "&amp;", "~", "�?", "�", "\|", "[ \s][?]"]
+    replaces = [" ", '"', " ", "", "", "&", " ", "", "", " ", " "]
+    replaces_counter = 0
 
-for pat in patterns:
-    #if pat in output_text:
-    output_text = re.sub(pat, replaces[replaces_counter], output_text)
-    replaces_counter += 1
-    
-# having trouble with removing carets via regex despite using escape chars. workaround.
-output_text = output_text.replace("^", "")
+    for pat in patterns:
+        #if pat in output_text:
+        output_text = re.sub(pat, replaces[replaces_counter], output_text)
+        replaces_counter += 1
+        
+    # having trouble with removing carets via regex despite using escape chars. workaround.
+    output_text = output_text.replace("^", "")
 
-output_file.write(output_text)
+    output_file.write(output_text)
 
-input_text.close()
-output_file.close()
+    input_text.close()
+    output_file.close()
 
-#print(input_text)
+if __name__ == "__main__":
+    app()
